45c1eedb928d2791b8be3418f3e5f713eaaea0bd
wesselversluis <wesselversluis@gmail.com>
2017-10-04 11:32:19 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-// Tell D3 to use the svg element in the html, set the margins for the bar-chart, for example: bottom not to small otherwise you won't see the labels
+// Tell D3 to use the svg element in the html, set the margins for the bar-chart, for example: bottom not to small otherwise you won't see the labels, /Declare x- and y-axis/ Add a group element to the svg/ add tooltip which returns the amount of speakers
 var svg = d3.select("svg"),
-    margin = {top: 20, right: 20, bottom: 30, left: 140},
+    margin = {top: 120, right: 20, bottom: 30, left: 120},
     width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom;
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+    y = d3.scaleLinear().rangeRound([height, 0]),
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
+    tool_tip = d3.tip().attr("class", "d3-tip").offset([-15, 0]).html(function(d) { return (d.speakers + " Speakers") })
 
-// Declare x- and y-axis
-var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-    y = d3.scaleLinear().rangeRound([height, 0]);
+// Load tsv data and get all the data-entries 
+d3.tsv("data.tsv", function (d) {
+    d.speakers = +d.speakers;
+    return d;
+}, function (error, data) {
+    if (error) throw error;
 
-// Add a group element to the svg
-var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// Load tsv data and return all data-entries
-d3.tsv("data.tsv", function(d) {
-  d.speakers = +d.speakers;
-  return d;
-}, function(error, data) {
-  if (error) throw error;
-
-// Return the letter from the .tsv data on the x-axis and return the value of the frequency of that day on the y-axis
+    
+// Return the language from the .tsv data on the x-axis and create a y-axis based on the maximum value in the data
   x.domain(data.map(function(d) { return d.language; }));
   y.domain([0, d3.max(data, function(d) { return d.speakers; })]);
 
@@ -32,22 +29,30 @@ d3.tsv("data.tsv", function(d) {
 // select the Group element and add y-axis aswell as text-attributes and declare how many ticks you want and changed from percentage to specific amount
   g.append("g")
       .attr("class", "axis axis--y")
-      .call(d3.axisLeft(y).ticks(20))
-    .append("text")
-      .attr("transform", "rotate(90)")
+      .call(d3.axisLeft(y).ticks(15))
+      .append("text")
       .attr("y", 6)
       .attr("dy", "0.71em")
       .attr("text-anchor", "end")
-      .text("Speakers");
 
-
-// select all alements in svg group, give them the class bar, load the data to create a rectangle based on the value of the data. Changed letter to day, because I changed the data
+// select all alements in svg group, give them the class bar, load the data to create a rectangle based on the value of the data. Give rects random color with fill. Added a tooltip on hover with mouseover/mouseout
   g.selectAll(".bar")
     .data(data)
-    .enter().append("rect")
+    .enter()
+      .append("rect")
       .attr("class", "bar")
       .attr("x", function(d) { return x(d.language); })
       .attr("y", function(d) { return y(d.speakers); })
       .attr("width", x.bandwidth())
-      .attr("height", function(d) { return height - y(d.speakers); });
+      .style("fill", function() { return "hsl(" + Math.random() * 360 + ",100%,50%)"})
+      .attr("height", function(d) { return height - y(d.speakers); })
+      .on('mouseover', tool_tip.show)
+      .on('mouseout', tool_tip.hide)
+
+    
+   //Call tooltip     
+   svg.call(tool_tip);
 });
+
+
+
